328f995604935e3ef28b5504a79bdf2af5f2fd1d
maku693 <me@maku693.net>
2022-04-10 00:44:13 +0900

    Import crates' identifiers to improve readability

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use std::sync::{Arc, RwLock};
+use std::{
+    sync::{Arc, RwLock},
+    thread::sleep,
+    time::Duration,
+};
 
 use anyhow::{Context, Result};
+use tokio::task;
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
 mod renderer;
 
@@ -8,11 +19,11 @@ mod renderer;
 async fn main() -> Result<()> {
     env_logger::init();
 
-    let event_loop = winit::event_loop::EventLoop::new();
+    let event_loop = EventLoop::new();
 
-    let window = winit::window::WindowBuilder::new()
+    let window = WindowBuilder::new()
         .with_title("Hello, world!")
-        .with_inner_size(winit::dpi::LogicalSize::<u32> {
+        .with_inner_size(LogicalSize::<u32> {
             width: 640,
             height: 360,
         })
@@ -23,19 +34,14 @@ async fn main() -> Result<()> {
 
     {
         let renderer = renderer.clone();
-        tokio::task::spawn_blocking(move || loop {
+        task::spawn_blocking(move || loop {
             renderer.read().unwrap().poll_device();
-            std::thread::sleep(std::time::Duration::from_millis(100));
+            sleep(Duration::from_millis(100));
         });
     }
 
     tokio::task::block_in_place(move || {
         event_loop.run(move |e, _, control_flow| {
-            use winit::{
-                event::{Event, WindowEvent},
-                event_loop::ControlFlow,
-            };
-
             *control_flow = ControlFlow::Poll;
 
             match e {
